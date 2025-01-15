fcfa89308e298eb686e6facf170e80b5467cfa0b
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-01-15 13:20:21 +0800

    use DTCM (really)

--- mk/opencm3.mk
+++ mk/opencm3.mk
@@ -24,7 +24,7 @@ ifeq ($(DEVICE),)
 $(warning no DEVICE specified for linker script generator)
 endif
 
-DEVICES_DATA ?= $(OPENCM3_DIR)/ld/devices.data
+DEVICES_DATA ?= ldscripts/devices.data
 
 genlink_family		:=$(shell $(OPENCM3_DIR)/scripts/genlink.py $(DEVICES_DATA) $(DEVICE) FAMILY)
 genlink_subfamily	:=$(shell $(OPENCM3_DIR)/scripts/genlink.py $(DEVICES_DATA) $(DEVICE) SUBFAMILY)
